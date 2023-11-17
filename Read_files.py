061d08f11a96dd292f009ace2a15dbd95fecc967
Giulia-dedo <78707453+Giulia-dedo@users.noreply.github.com>
2023-11-17 19:53:23 +0100

    Update Read_files.py
    w

--- Read_files.py
+++ Read_files.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 
 startpath = Path(r'C:\Users\gdonno\Desktop\Hackathon\innovaid_hackathon_anima\input')
+output_path = Path(r'')
 
 filelist = list(startpath.glob('**/*.csv') )      
 
@@ -13,12 +14,22 @@ for elem in filelist[0:1]:
     complete_ds = pd.read_csv(elem)
     complete_ds.describe()
 
+    # order by timestamp
+    complete_ds = complete_ds.sort_values(by='TIMESTAMP')
 
-    subset = complete_ds.loc[ :, [ 'TIMESTAMP', 'IMAGE', 'IMAGE_POSITION', 'IMAGE_TYPE']] 
+    # calculate timediff 
+    diff_series = complete_ds['TIMESTAMP'].diff().dropna() .astype(int) # Drop NaN for the first element
+    time_sampl = diff_series.value_counts().idxmax()
 
-    
+    # create new timestamp
+    complete_ds['TIME'] = complete_ds.index*time_sampl
+
+    # create subset, save
+    subset = complete_ds.loc[ :, [ 'TIME', 'IMAGE_POSITION', 'IMAGE_TYPE', 'SCENE_INDEX']] 
+
+    output_path = Path(str(elem).replace('input', 'subset') )
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+    subset.to_csv(output_path)
 
 
-
-
-
+# %%
